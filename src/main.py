bd9c0359227828edb1223a77384abc18a2f802da
Eitan <eitanrosenzvaig@gmail.com>
2015-11-17 23:42:18 -0300

    main

--- src/main.py
+++ src/main.py
@@ -2,52 +2,56 @@
 # probar las distintas partes del codigo
 
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage import convolve
+from sklearn import linear_model, datasets, metrics
+from sklearn.cross_validation import train_test_split
+from sklearn.neural_network import BernoulliRBM
+from sklearn.pipeline import Pipeline
+import numpy as np
 import cv2
 import os
 from Constants import Constants
 from ImagesProcessor import ImagesProcessor
 from RandomForest import RandomForest
+from LogisticRegression import LogisticRegression
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.cross_validation import KFold
 from sklearn.cross_validation import train_test_split
 from sklearn.grid_search import GridSearchCV
 from sklearn.metrics import classification_report
+from Ensemble import Ensemble
 
 import rlcompleter, readline
 readline.parse_and_bind('tab:complete')
 
-ip = ImagesProcessor('../imgs/test/medium/', training=True)
+ip = ImagesProcessor()
+images, y = ip.getImages('../imgs/test/medium/', training=True)
 
-images = ip.getImages()
+X_train, X_test, y_train, y_test = train_test_split(images, y, test_size=0.3)
+X_train, y_train = ip.transformImages(X_train, y_train)
+X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=0.6)
+
+ensemble = Ensemble()
+ensemble.fit_small(X_train, y_train)
+X_validation_predictions = ensemble.predict_small(X_validation)
+ensemble.fit_big(X_validation_predictions, y_validation)
+
+X_test_predictions = ensemble.predict_small(X_test)
+y_hat = ensemble.predict_big(X_test_predictions)
 
 
-# def crossValidation(X, Y):
-#     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=Constants.TEST_SIZE, random_state=Constants.RANDOM_STATE)
-#     kf = kf = KFold(len(X_train), n_folds=30, shuffle=False, random_state=Constants.RANDOM_STATE)
-#     for train_index, test_index in kf:
-#         print("TRAIN:", train_index, "TEST:", test_index)
-
-
-#ip = ImagesProcessor('../imgs/test/small/', training=True)
-
-# textures = ip.getTextureFeature(5, 12)
-# y = ip.getImagesClass()
-
-# r = RandomForest(textures, y)
-# r.fit()
-# r.score()
-
-# pca, norm, ds = ip.getPcaFeatures(10, (100, 100))
-# y = ip.getImagesClass()
-# pr = RandomForest(ds, y)
-# pr.fit()
-# pr.score()
-
-# rbm, ds = ip.getBernulliRBM(20, (256, 256), n_iter=20, learning_rate=0.005)
-# y = ip.getImagesClass()
-# pr = RandomForest(ds, y)
-# pr.fit()
-# pr.score()
-
+#plt.figure(figsize=(14.2, 10))
+#for i, comp in enumerate(rbm1.components_):
+    #plt.subplot(10, 10, i + 1)
+    #plt.imshow(comp.reshape((56, 56)), cmap=plt.cm.gray_r, interpolation='nearest')
+    #plt.xticks(())
+    #plt.yticks(())
+#
+#plt.suptitle('100 components extracted by RBM', fontsize=16)
+#plt.subplots_adjust(0.08, 0.02, 0.92, 0.85, 0.08, 0.23)
+#
+#plt.show()
 
 #images = ip.getImages()
 #gimages = ip.getImagesWithGrayHistogramEqualized()
@@ -56,6 +60,13 @@ images = ip.getImages()
 ## Como mostrar una foto
 #image = cv2.imread('../imgs/test/cat.0.jpg')
 #cv2.imshow('color_image',image)
+#cv2.waitKey(0)                 # Waits forever for user to press any key
+#cv2.destroyAllWindows()        # Closes displayed windows
+
+#image = ds[0]
+#image = cv2.resize(image, (106, 106))
+#
+#cv2.imshow('color_image', image)
 #cv2.waitKey(0)                 # Waits forever for user to press any key
 #cv2.destroyAllWindows()        # Closes displayed windows
 
