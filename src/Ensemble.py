7359c387e26f81c983d9c913ca55a6c06e51d22a
Martin Forte <martin.forte@mercadolibre.com>
2015-11-17 23:58:06 -0300

    Agrego multithreading al preprocessing

--- src/Ensemble.py
+++ src/Ensemble.py
@@ -4,7 +4,7 @@ from sklearn.neural_network import BernoulliRBM
 from sklearn import linear_model
 from RandomForest import RandomForest
 from ImagesProcessor import ImagesProcessor
-
+import threading
 
 class Ensemble(object):
 
@@ -20,14 +20,54 @@ class Ensemble(object):
         self.ip = ImagesProcessor()
 
     def fit_small(self, images, y):
+        t_pc = threading.Thread(target=self._fit_small_pc, args = (images[:], y))
+        t_pc.daemon = True
+        t_pc.start()
+
+        t_rbm = threading.Thread(target=self._fit_small_rbm, args = (images[:], y))
+        t_rbm.daemon = True
+        t_rbm.start()
+
+        t_t10_10 = threading.Thread(target=self._fit_small_texture1, args = (images[:], y, self.texture_10_10_randomForest, 5, 10, 2000))
+        t_t10_10.daemon = True
+        t_t10_10.start()
+
+        t_t5_10 = threading.Thread(target=self._fit_small_texture1, args = (images[:], y, self.texture_5_10_randomForest, 5, 10, 2000))
+        t_t5_10.daemon = True
+        t_t5_10.start()
+
+        t_pc.join()
+        t_rbm.join()
+        t_t10_10.join()
+        t_t5_10.join()
+
+        # self._fit_small_pc(images[:], y)
+        # self._fit_small_rbm(images[:], y)
+        # self._fit_small_texture1(images[:], y, self.texture_5_10_randomForest, 5, 10, 2000)
+        # self._fit_small_texture2(images[:], y, self.texture_10_10_randomForest, 10, 10, 2000)
+
+    # FIXE: unificar estas dos funciones. No le gusta pasar el estimador como atributo
+    def _fit_small_texture1(self, images, y, estimator, radius, points, n_estimators):
+        print("TEXTURE %d %d" % (radius, points))
+        ds = self.ip.getTextureFeature(images, radius, points)
+        self.texture_5_10_randomForest = RandomForest(ds, y, n_estimators=n_estimators)
+        self.texture_5_10_randomForest.fit()
+
+    def _fit_small_texture2(self, images, y, estimator, radius, points, n_estimators):
+        print("TEXTURE %d %d" % (radius, points))
+        ds = self.ip.getTextureFeature(images, radius, points)
+        self.texture_10_10_randomForest = RandomForest(ds, y, n_estimators=n_estimators)
+        self.texture_10_10_randomForest.fit()
+
+    def _fit_small_pc(self, images, y):
         print("PCA RANDOM FOREST")
         ds = self.ip.getImagesWithGrayHistogramEqualized(images=images)
         self.pca_randomForest_pca, self.pca_randomForest_norm, ds = self.ip.getPcaFeatures(ds, 20, (56, 56))
         self.pca_randomForest = RandomForest(ds, y, n_estimators=2000)
         self.pca_randomForest.fit()
 
+    def _fit_small_rbm(self, ds, y):
         print("RBM LR")
-        ds = images[:]
         ds = self.ip.getImagesAsDataset(ds, (56, 56))
         ds = (ds - np.min(ds, 0)) / (np.max(ds, 0) + 0.0001)
         self.rbm_lr_rbm = BernoulliRBM(random_state=0, verbose=True)
@@ -37,16 +77,6 @@ class Ensemble(object):
         logistic = linear_model.LogisticRegression()
         self.rbm_lr = Pipeline(steps=[('rbm', self.rbm_lr_rbm), ('lr', logistic)])
         self.rbm_lr.fit(ds, y)
-
-        print("TEXTURE 10 10")
-        ds = self.ip.getTextureFeature(images, 10, 10)
-        self.texture_10_10_randomForest = RandomForest(ds, y, n_estimators=2000)
-        self.texture_10_10_randomForest.fit()
-
-        print("TEXTURE 5 10")
-        ds = self.ip.getTextureFeature(images, 5, 10)
-        self.texture_5_10_randomForest = RandomForest(ds, y, n_estimators=2000)
-        self.texture_5_10_randomForest.fit()
 
     def fit_big(self, ds, y):
         self.ensemble_logistic_regression = linear_model.LogisticRegression()
